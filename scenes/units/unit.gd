163a8cc2e488a0ba2426ed9784ae651bffff5b75
Jerrywang993 <wang.tianye2@northeastern.edu>
2025-07-04 10:39:33 -0400

    feat: add move speed initializing variable and call the move(unfinshed) function in _process to prepare for unit movement

--- scenes/units/unit.gd
+++ scenes/units/unit.gd
@@ -2,8 +2,12 @@ extends Node2D
 class_name Unit
 
 var target_global_position: Vector2i
+var move_speed: float = 100.0
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("left_mouse_click"):
 		var mouse_grid_position := GridManager.get_mouse_grid_position()
 		target_global_position = GridManager.get_world_position(mouse_grid_position)
+
+func _process(delta: float) -> void:
+	move(target_global_position, delta)
