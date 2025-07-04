5d913898745573e9428655c98c6bde181f8ea893
Jerrywang993 <wang.tianye2@northeastern.edu>
2025-07-03 20:30:48 -0400

    feat: add mouse grid position change detection and update and then use GridManager to convert grid position to world position

--- scenes/ui/grid_indicator.gd
+++ scenes/ui/grid_indicator.gd
@@ -4,3 +4,6 @@ var mouse_grid_position: Vector2i
 
 func _process(_delta: float) -> void:
     var new_mouse_grid_position = GridManager.get_mouse_grid_position()
+    if new_mouse_grid_position != mouse_grid_position:
+        mouse_grid_position = new_mouse_grid_position
+        global_position = GridManager.get_world_position(mouse_grid_position)
