eaad2c6a5e4a123513b8e19fad1256de514dd9ee
Jerrywang993 <wang.tianye2@northeastern.edu>
2025-07-06 10:40:19 -0400

    feat: set a_star cell size and disable diagonal movement

--- grids/nav_layer.gd
+++ grids/nav_layer.gd
@@ -9,3 +9,5 @@ func _ready() -> void:
 func initialize() -> void:
 	a_star = AStar2D.new()
 	a_star.region = get_used_rect()
+	a_star.cell_size = tile_set.tile_size
+	a_star.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_NEVER
