3147344d6013c51ac11d69fa142f4a7df0b4af72
Jerrywang993 <wang.tianye2@northeastern.edu>
2025-07-09 17:15:21 -0400

    fix: change a_star initial type assignment from AStar2D to AStarGrid2D for component consistency

--- grids/nav_layer.gd
+++ grids/nav_layer.gd
@@ -8,7 +8,7 @@ func _ready() -> void:
 	GridManager.nav_layer = self
 
 func initialize() -> void:
-	a_star = AStar2D.new()
+	a_star = AStarGrid2D.new()
 	a_star.region = get_used_rect()
 	a_star.cell_size = tile_set.tile_size
 	a_star.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_NEVER
