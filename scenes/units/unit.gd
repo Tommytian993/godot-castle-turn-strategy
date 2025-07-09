6149725635d4dbd9d50983c3b551a9e9b3dd908f
Jerrywang993 <wang.tianye2@northeastern.edu>
2025-07-08 21:02:56 -0400

    feat: add grid_position getter property that converts global position to grid coordinates

--- scenes/units/unit.gd
+++ scenes/units/unit.gd
@@ -3,11 +3,14 @@ class_name Unit
 
 var target_global_position: Vector2i
 var move_speed: float = 100.0
+var grid_position: Vector2i:
+	get: return GridManager.get_grid_position(global_position)
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("left_mouse_click"):
 		var mouse_grid_position := GridManager.get_mouse_grid_position()
 		target_global_position = GridManager.get_world_position(mouse_grid_position)
+		GridManager.get_nav_world_path(global_position, target_global_position)
 
 func _process(delta: float) -> void:
 	move(target_global_position, delta)
