a0f4d91db092890239f6a3a6f76c9aabb2530708
Jerrywang993 <wang.tianye2@northeastern.edu>
2025-07-09 17:27:40 -0400

    fix: add bounds check in get_nav_grid_path to avoid out-of-bounds errors in pathfinding

--- scenes/autoloads/grid_manager.gd
+++ scenes/autoloads/grid_manager.gd
@@ -14,6 +14,9 @@ func get_mouse_grid_position() -> Vector2i:
 	return get_grid_position(get_mouse_world_position())
 
 func get_nav_grid_path(start_grid_position: Vector2i, end_grid_position: Vector2i) -> Array[Vector2i]:
+	if not nav_layer.a_star.is_in_bounds(start_grid_position) or not nav_layer.a_star.is_in_bounds(end_grid_position):
+		push_error("Pathfinding point out of bounds: start=%s, end=%s" % [start_grid_position, end_grid_position])
+		return []
 	return nav_layer.a_star.get_id_path(start_grid_position, end_grid_position)
 
 func get_nav_world_path(start_world_position: Vector2, end_world_position: Vector2) -> Array[Vector2]:
