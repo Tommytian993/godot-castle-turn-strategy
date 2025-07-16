3eedb2774f2c02c198450ecc2316cabdd64f5878
Jerrywang993 <wang.tianye2@northeastern.edu>
2025-07-16 17:38:52 -0400

    feat: add is_occupied_by_unit() method to GridData for unit occupancy check

--- grids/grid_data.gd
+++ grids/grid_data.gd
@@ -2,3 +2,7 @@ class_name GridData
 
 var walkable: bool = true
 var occupied_unit: Unit = null
+
+# Returns true if the grid is occupied by a unit
+func is_occupied_by_unit() -> bool:
+	return occupied_unit != null
