9d6cf11f599c8e598880e5bcb9e755e373be5da7
Jerrywang993 <wang.tianye2@northeastern.edu>
2025-07-20 21:34:18 -0400

    feat: add start_action method to MoveAction
    - Override start_action method from BaseAction
    - Calculate navigation path using GridManager
    - Store path from current position to target position

--- scenes/actions/move_action.gd
+++ scenes/actions/move_action.gd
@@ -3,3 +3,7 @@ class_name MoveAction
 
 var path: Array[Vector2]
 var move_speed: float = 100.0
+
+func start_action(target_grid_position: Vector2i, on_action_finished: Callable) -> void:
+	super.start_action(target_grid_position, on_action_finished)
+	path = GridManager.get_nav_world_path(unit.grid_position, target_grid_position)
