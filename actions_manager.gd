8f37bd5870ae4eb1a6876ce30eaff8ad327b85c9
Jerrywang993 <wang.tianye2@northeastern.edu>
2025-07-20 20:51:22 -0400

    feat: add get_action method for action lookup

--- actions_manager.gd
+++ actions_manager.gd
@@ -6,3 +6,9 @@ var actions: Array[BaseAction] # all user skills
 func _ready() -> void:
 	for action: BaseAction in get_children():
 		actions.append(action)
+
+func get_action(action_id: String) -> BaseAction:
+	var results = actions.filter(func(action: BaseAction) -> bool:
+		return action.action.id == action_id
+	)
+	return results[0] if results.size() > 0 else null
