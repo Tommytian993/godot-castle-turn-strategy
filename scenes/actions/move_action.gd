74254d4375831b2fd9722e16f34c60cac01736ef
Jerrywang993 <wang.tianye2@northeastern.edu>
2025-07-21 21:47:34 -0400

    refactor: move the _process function inside move_action and add checks and corresponding returns for not activating skill and finishing the path

--- scenes/actions/move_action.gd
+++ scenes/actions/move_action.gd
@@ -9,4 +9,16 @@ func start_action(target_grid_position: Vector2i, on_action_finished: Callable) 
 	path = GridManager.get_nav_world_path(unit.grid_position, target_grid_position)
 
 func move(target_position: Vector2, delta: float) -> void:
-	unit.global_position = global_position.move_toward(target_position, move_speed * delta)
+	unit.global_position = unit.global_position.move_toward(target_position, move_speed * delta)
+
+func _process(delta: float) -> void:
+	# 如果技能没激活，则返回
+	if not is_active:
+		return
+	if path and not path.is_empty():
+		move(path[0], delta)
+		if unit.global_position.distance_to(path[0]) < 1.0:
+			path.remove_at(0)
+	# 如果路径为空，则完成动作
+	else:
+		finish_action()
