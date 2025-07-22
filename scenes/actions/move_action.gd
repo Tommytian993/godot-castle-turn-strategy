2d62ff539aed5d0d4b243433af963ce7d79997bd
Jerrywang993 <wang.tianye2@northeastern.edu>
2025-07-21 21:43:05 -0400

    refactor: move the move function inside the move_action script and call it via unit object

--- scenes/actions/move_action.gd
+++ scenes/actions/move_action.gd
@@ -7,3 +7,6 @@ var move_speed: float = 100.0
 func start_action(target_grid_position: Vector2i, on_action_finished: Callable) -> void:
 	super.start_action(target_grid_position, on_action_finished)
 	path = GridManager.get_nav_world_path(unit.grid_position, target_grid_position)
+
+func move(target_position: Vector2, delta: float) -> void:
+	unit.global_position = global_position.move_toward(target_position, move_speed * delta)
