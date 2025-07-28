bf3f04b95976a6305bbe36eba102146dbb2b8226
Jerrywang993 <wang.tianye2@northeastern.edu>
2025-07-28 02:12:51 -0400

    docs: add more comments explaining the action contianer reference initialization

--- scenes/ui/unit_actions_ui.gd
+++ scenes/ui/unit_actions_ui.gd
@@ -3,6 +3,7 @@ class_name UnitActionsUI
 
 @export var action_card_ui: PackedScene
 #按住control 拖入
+#onready is more safer, it will wait for node to be ready, and check type at compile time
 @onready var action_container: HBoxContainer = $MarginContainer/ActionContainer
 #没按control
 #$MarginContainer/ActionContainer
