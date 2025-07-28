fa59cb9f6473f8af91ef5803350664e8752cf993
Jerrywang993 <wang.tianye2@northeastern.edu>
2025-07-28 02:11:39 -0400

    feat: add typed action container reference to UnitActionsUI

--- scenes/ui/unit_actions_ui.gd
+++ scenes/ui/unit_actions_ui.gd
@@ -2,3 +2,7 @@ extends MarginContainer
 class_name UnitActionsUI
 
 @export var action_card_ui: PackedScene
+#按住control 拖入
+@onready var action_container: HBoxContainer = $MarginContainer/ActionContainer
+#没按control
+#$MarginContainer/ActionContainer
