962ef725d0a1abae551713c5ba84ca72a2339942
Jerrywang993 <wang.tianye2@northeastern.edu>
2025-10-02 11:29:11 -0400

    feat: add the extends keyword in player to inherent from the character class

--- Scripts/Character/player.gd
+++ Scripts/Character/player.gd
@@ -1,9 +1,10 @@
-extends Node
+class_name Player
+extends Character
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	super._ready()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
