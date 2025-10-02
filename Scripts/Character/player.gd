8d5c18d34f96c27247111889cde06c27496b3006
Jerrywang993 <wang.tianye2@northeastern.edu>
2025-10-02 11:59:11 -0400

    feat: adjust player script settings

--- Scripts/Character/player.gd
+++ Scripts/Character/player.gd
@@ -2,9 +2,6 @@ class_name Player
 extends Character
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	super._ready()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
