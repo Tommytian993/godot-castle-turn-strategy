6294dbee6dc27ac3b1501b042531e701d9629063
Jerrywang993 <wang.tianye2@northeastern.edu>
2025-10-02 19:56:50 -0400

    feat: add mouse aiming to Player class

--- Scripts/Character/player.gd
+++ Scripts/Character/player.gd
@@ -2,6 +2,12 @@ class_name Player
 extends Character
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# 每帧调用。'delta' 是自上一帧以来经过的时间。
 func _process(delta: float) -> void:
+	# 从 WASD 键获取移动输入
 	move_input = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	
+	# 计算鼠标位置用于瞄准
+	var mouse_pos: Vector2 = get_global_mouse_position()
+	# 计算从玩家位置到鼠标光标的朝向
+	look_direction = global_position.direction_to(mouse_pos)
