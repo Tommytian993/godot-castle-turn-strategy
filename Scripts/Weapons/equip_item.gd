e771843935e58d8e434ebbc0ab408cc45d11f6d1
Jerrywang993 <wang.tianye2@northeastern.edu>
2025-10-03 15:14:26 -0400

    feat: add EquipItem class with method structure

--- Scripts/Weapons/equip_item.gd
+++ Scripts/Weapons/equip_item.gd
@@ -6,3 +6,21 @@ var last_use_time: float
 var aim_angle: float
 var owner_character: Character
 var can_use: bool = true
+
+func _process(delta: float):
+	pass
+
+func set_aim_direction(aim_dir: Vector2):
+	pass
+
+func _equip():
+	pass
+
+func _unequip():
+	pass
+
+func _try_use() -> bool:
+	pass
+
+func _use():
+	pass
