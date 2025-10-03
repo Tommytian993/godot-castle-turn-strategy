2257a6787ccb346c0846fa70aed9f15793b50c3a
Jerrywang993 <wang.tianye2@northeastern.edu>
2025-10-02 22:21:48 -0400

    fix: correct export syntax in character_visuals

--- Scripts/Character/character_visual.gd
+++ Scripts/Character/character_visual.gd
@@ -1,19 +1,19 @@
 class_name CharacterVisual
 extends Sprite2D
 
-export var character: Character
+@export var character: Character
 
-export var wobble_speed: float = 15
-export var wobble_amount: float = 5
+@export var wobble_speed: float = 15
+@sexport var wobble_amount: float = 5
 
 func _process(delta: float):
-    # sprite flipping
-    flip_h = character.look_direction.x < 0
+	# sprite flipping
+	flip_h = character.look_direction.x < 0
 
-    # move wobble
-    var target_rot: float = 0
-    if character.velocity.length() > 1:
-        var time = Time.get_unix_time_from_system()
-        target_rot = sin(time * wobble_speed) * wobble_amount
+	# move wobble
+	var target_rot: float = 0
+	if character.velocity.length() > 1:
+		var time = Time.get_unix_time_from_system()
+		target_rot = sin(time * wobble_speed) * wobble_amount
 
-    rotation_degrees = lerpf(rotation_degrees, target_rot, delta)
+	rotation_degrees = lerpf(rotation_degrees, target_rot, delta)
